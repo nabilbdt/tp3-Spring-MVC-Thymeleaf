012ae5681e27c3d64aed3205c6aa8bf0dd34520c
nabilbdt <nabilbayad969@gmail.com>
2025-11-25 21:51:11 +0100

    mvc done

--- src/main/java/jee/mundia/tp3springmvcthymeleaf/Tp3SpringMvcThymeleafApplication.java
+++ src/main/java/jee/mundia/tp3springmvcthymeleaf/Tp3SpringMvcThymeleafApplication.java
@@ -9,7 +9,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class Tp3SpringMvcThymeleafApplication {
 
     public static void main(String[] args) {
--- src/main/java/jee/mundia/tp3springmvcthymeleaf/web/ProductController.java
+++ src/main/java/jee/mundia/tp3springmvcthymeleaf/web/ProductController.java
@@ -1,12 +1,15 @@
 package jee.mundia.tp3springmvcthymeleaf.web;
 
+import jakarta.validation.Valid;
 import jee.mundia.tp3springmvcthymeleaf.entities.Product;
 import jee.mundia.tp3springmvcthymeleaf.repository.ProductRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,6 +28,18 @@ public class ProductController {
     public String home(){
         return "redirect:/index";
     }
+    @GetMapping("/newProduct")
+    public String newProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+    @PostMapping("saveProduct")
+    public  String saveProduct(@Valid Product product, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) return "new-product";
+        productRepository.save(product);
+        return  "redirect:/index";
+    }
+
     @GetMapping("/delete")
     public  String delete(Long id){
         productRepository.deleteById(id);
